118cbb41a36287b2bc3eed80b99e11c225a8ae2b
Taliesin Sisson <taliesins@interxion.com>
2014-12-01 17:45:40 +0000

    Add build for project

---
+++ build.ps1
@@ -0,0 +1,32 @@
+﻿param(
+  [Parameter(Position=0,Mandatory=0)]
+  [string]$buildFile = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\build\build.ps1",
+  [Parameter(Position=1,Mandatory=0)]
+  [string[]]$taskList = @(),
+  [Parameter(Position=2,Mandatory=0)]
+  [string]$framework = '4.5',
+  [Parameter(Position=3,Mandatory=0)]
+  [switch]$docs = $false,
+  [Parameter(Position=4,Mandatory=0)]
+  [System.Collections.Hashtable]$parameters = @{},
+  [Parameter(Position=5, Mandatory=0)]
+  [System.Collections.Hashtable]$properties = @{}
+)
+
+$rootPath = (Split-Path -parent $MyInvocation.MyCommand.Definition)
+$buildPath = (Resolve-Path $rootPath\build)
+
+$psakeLocations = @(Get-ChildItem $rootPath\build\* -recurse -include psake.ps1)
+$psakeModule = $psakeLocations[0].FullName
+$psakeModuleDirectory = $psakeLocations[0].DirectoryName
+
+. $buildPath\bootstrap.ps1 $buildPath
+. $psakeModule -buildFile $buildFile -taskList $taskList -framework $framework -docs:$docs -parameters $parameters -properties $properties -scriptPath $psakeModuleDirectory
+. $psakeModule -buildFile $buildFile -taskList Package -framework $framework -docs:$docs -parameters $parameters -properties $properties -scriptPath $psakeModuleDirectory
+
+
+if($env:BUILD_NUMBER) {
+  [Environment]::Exit($lastexitcode)
+} else {
+  exit $lastexitcode
+}
---
+++ build/overrides.ps1
@@ -0,0 +1,18 @@
+﻿# 
+# Copyright (c) 2011-2012, Toji Project Contributors
+# 
+# Dual-licensed under the Apache License, Version 2.0, and the Microsoft Public License (Ms-PL).
+# See the file LICENSE.txt for details.
+# 
+
+# This file is where you can override any settings specified in the other scripts.
+# These settings can also be used to override conventions such as solution name and nuspec file.
+
+properties {
+    Write-Output "Loading override settings"
+    $solution.name = "Talifun-Web"
+    $solution.file = "$($base.dir)\$($solution.name).sln"
+    $talifunweb = "Talifun.Crusher.dll"
+    $talifunwebAssemblies = @("Talifun.FileWatcher.dll","AjaxMin.dll", "dotless.ClientOnly.dll", "EcmaScript.NET.dll", "Yahoo.Yui.Compressor.dll")
+    $ilmerge.targets["$talifunweb"] = $talifunwebAssemblies
+}
