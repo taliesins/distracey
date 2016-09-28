7d6d5a67361b15551e902fb1e0ac19e49eac3c1a
Taliesin Sisson <taliesins@yahoo.com>
2016-09-28 22:41:17 +0100

    Remove dead code

--- src/Distracey.Agent.SystemWeb/Session/IHttpContextSessionContextExtensions.cs
+++ src/Distracey.Agent.SystemWeb/Session/IHttpContextSessionContextExtensions.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Web;
-using Distracey.Agent.SystemWeb.Session.OperationCorrelation;
-using Distracey.Agent.SystemWeb.Session.SessionIdentifier;
-using Distracey.Common.Session;
-using Distracey.Common.Session.OperationCorrelation;
-using Distracey.Common.Session.SessionAudit;
-using Distracey.Common.Session.SessionIdentifier;
+﻿using System.Web;
 
 namespace Distracey.Agent.SystemWeb.Session
 {
@@ -13,20 +6,7 @@ namespace Distracey.Agent.SystemWeb.Session
     {
         public static void UseHttpContextSessionContext()
         {
-            //SessionContext.OperationCorrelationManager = new Lazy<OperationCorrelationManager>(OperationCorrelationManagerFactory);
-            //SessionContext.SessionContainer = new Lazy<ISessionContainer>(SessionContainerFactory);
             HttpApplication.RegisterModule(typeof(HttpContextSessionContainerHttpModule));
-        }
-
-        private static ISessionContainer SessionContainerFactory()
-        {
-            return new InMemorySessionContainer(new HttpContextSessionIdentifierStorage(new CallContextSessionIdentifierStorage()), new SessionAuditStorageLogger(), TimeSpan.FromSeconds(10));
-        }
-
-        private static OperationCorrelationManager OperationCorrelationManagerFactory()
-        {
-            var httpContextOperationCorrelationManager = new OperationCorrelationManager(new OperationStack(new HttpContextOperationStackStorage()));
-            return httpContextOperationCorrelationManager;
         }
     }
 }
